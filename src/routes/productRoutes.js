51c0e544d0b5d7b6dc43f679501f8efe8b35ff53
Eissanoor <eissaanoor@gmail.com>
2025-05-28 20:41:54 +0500

    Add new route for product retrieval using version 5 controller. Introduced '/v5' endpoint in productRoutes to utilize productControllerV5 for fetching all products, enhancing versioning support in the API.

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const productController = require('../controllers/productControllerV4');
+const productControllerV5 = require('../controllers/productControllerV5');
 const router = express.Router();
 
 // GET /api/products - Get all products for testing
 router.get('/', productController.getAllProducts);
-
+router.get('/v5', productControllerV5.getAllProducts);
 module.exports = router;
